e4795d639cd98f19becfdfa924f04341e0c31523
Robert Allred <rallred84@gmail.com>
2023-03-01 21:25:30 -0600

    added random mole and whack a mole functions to index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 let score = 0;
+let scoreBoard = document.getElementById("score");
+
+let allHoles = document.getElementsByClassName("hole");
+
+let randomHole;
+
+function toggleMole() {
+  randomHole = Math.floor(Math.random() * allHoles.length);
+  allHoles[randomHole].classList.toggle("mole");
+}
+
+function whackMole(tapHole) {
+  if (tapHole.target.matches(".mole")) {
+    tapHole.target.classList.toggle("mole"); //Doesnt allow multiple scores off one target
+    score += 1;
+    scoreBoard.innerText = score;
+  }
+}
+
+setInterval(toggleMole, 400);
+
+let gameArea = document.getElementById("whack-a-mole");
+gameArea.addEventListener("click", whackMole);
